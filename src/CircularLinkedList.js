9d4f72201385679185492281e4dade13f3e293a7
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-10-03 14:30:22 +0900

    added constructors and node classes

--- src/CircularLinkedList.js
+++ src/CircularLinkedList.js
@@ -1,5 +1,15 @@
-class CircularLinkedList {}
+class CircularLinkedList {
+    constructor(value){
+        if(value !== undefined){
+        this.sentinel = new Node(value);
+}
 
-
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
 
 module.exports = CircularLinkedList;
--- src/DoublyLinkedList.js
+++ src/DoublyLinkedList.js
@@ -1,6 +1,18 @@
 class DoublyLinkedList {
+    constructor(value){
+        if(value !== undefined){
+        this.head = new Node(value);
+        this.tail = this.head;
+    }
+
 }
 
-
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
 
 module.exports = DoublyLinkedList;
